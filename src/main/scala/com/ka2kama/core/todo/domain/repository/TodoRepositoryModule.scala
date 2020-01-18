15e15b6f5e283c6f0a2bab65dbdfd3cf8b54a1a6
ka2kamaboko <49121261+ka2kamaboko@users.noreply.github.com>
2020-01-18 23:53:30 +0900

    mem

--- src/main/scala/com/ka2kama/core/todo/domain/repository/TodoRepositoryModule.scala
+++ src/main/scala/com/ka2kama/core/todo/domain/repository/TodoRepositoryModule.scala
@@ -6,10 +6,10 @@ import net.codingwell.scalaguice.ScalaPrivateModule
 private[todo] class TodoRepositoryModule extends ScalaPrivateModule {
   override def configure(): Unit = {
 //    bind[TodoRepository].to[TodoRepositoryByAnorm]
-    bind[TodoRepository].to[TodoRepositoryByMongoDB]
+//    bind[TodoRepository].to[TodoRepositoryByMongoDB]
 //    bind[TodoRepository].to[TodoRepositoryByScalikeJDBC]
 //    bind[TodoRepository].to[TodoRepositoryBySlick]
-//    bind[TodoRepository].to[TodoRepositoryOnMemory]
+    bind[TodoRepository].to[TodoRepositoryOnMemory]
 
     expose[TodoRepository]
 
