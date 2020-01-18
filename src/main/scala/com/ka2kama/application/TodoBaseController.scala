5d6a3b1cd917657fd9c860cefa29102a80129aca
ka2kamaboko <49121261+ka2kamaboko@users.noreply.github.com>
2020-01-18 17:37:01 +0900

    controller

--- src/main/scala/com/ka2kama/application/TodoBaseController.scala
+++ src/main/scala/com/ka2kama/application/TodoBaseController.scala
@@ -1,24 +1,11 @@
 package com.ka2kama.application
 
-import io.circe.Encoder
-import io.circe.syntax._
 import javax.inject.Inject
 import play.api.Logging
-import play.api.http.Writeable
 import play.api.libs.circe.Circe
-import play.api.mvc.{AbstractController, ControllerComponents, Result}
+import play.api.mvc._
 
 abstract class TodoBaseController @Inject()(cc: ControllerComponents)
     extends AbstractController(cc)
     with Logging
-    with Circe {
-
-  implicit class OptionToResult[A](val self: Option[A]) {
-
-    def toJsonResult(implicit encoder: Encoder[A]): Result =
-      self.map(_.asJson).toResult
-
-    def toResult(implicit writeable: Writeable[A]): Result =
-      self.fold(NotFound: Result)(Ok(_))
-  }
-}
+    with Circe
