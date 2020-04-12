7a5680dac37e3f1ebb6e43ac2d06bee400deecb3
ka2kama <ka2kama@outlook.jp>
2020-04-12 13:52:18 +0900

    task

--- web/src/main/scala/com/ka2kama/todolist/web/ListController.scala
+++ web/src/main/scala/com/ka2kama/todolist/web/ListController.scala
@@ -15,11 +15,10 @@ final class ListController @Inject() (listService: ListService, cc: ControllerCo
   def list: Action[AnyContent] = ActionAsync {
     logger.info("list: ")
 
-    val result = for {
-      todos <- listService.list
-    } yield todos.asJson
+    val todosT     = listService.list
+    val todosJsonT = todosT.map(_.asJson)
 
-    result.map(Ok(_))
+    todosJsonT.map(Ok(_))
   }
 
   def get(id: Long): Action[AnyContent] = ActionAsync {
@@ -34,29 +33,6 @@ final class ListController @Inject() (listService: ListService, cc: ControllerCo
   def stream: Action[AnyContent] = Action {
     logger.info("stream: ")
 
-    //implicit val system: ActorSystem = ActorSystem("QuickStart")
-    //implicit val ec: ExecutionContextExecutor = system.dispatcher
-
-//    val source = Source(1 to 3000)
-//    //val sink       = Sink.fold[Int, Int](0)(_ + _)
-//    val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
-//    val result = factorials
-//      .zipWith(Source(0 to 10000))((num, idx) => s"$idx! = $num\n")
-//      .map(ByteString(_))
-//    //.throttle(1, 1.second)
-//
-//    Ok.streamed(result, None, inline = false, Some("test.txt"))
-//    val sum = source.runWith(sink)
-//
-//    sum.map(Ok(_))
-//    val file                          = new java.io.File("/tmp/fileToServe.pdf")
-//    val path: java.nio.file.Path      = file.toPath
-//    val source: Source[ByteString, _] = FileIO.fromPath(path)
-//
-//    Result(
-//      header = ResponseHeader(200, Map.empty),
-//      body = HttpEntity.Streamed(source, None, Some("application/pdf"))
-//    )
     Ok.sendFile(Environment.simple().getFile("/tmp/fileToServe.pdf"))
   }
 }
